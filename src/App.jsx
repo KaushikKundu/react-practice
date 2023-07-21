3be4b18d8271d70a24006945556aeecd40f81323
Kaushik Kundu <kaushikkundu774@gmail.com>
2023-07-21 10:53:19 +0530

    Excuses generator project is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import useBoolean from "./useBoolean";
+import Axios from "axios";
 
 function App() {
-  const [boolean, { setToggle, setTrue, setFalse }] = useBoolean();
+  const [excuse, setExcuse] = useState("");
+  const [excuseType, setExcuseType] = useState("party");
+
+  //dynamic values are sent to api call link which fetches excuses of that type. "ExcuseType" state has been declared to store the type. whenever the button is clicked , excuse is changed of that type.
+  function generateRandomExcuse() {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuseType}`).then(
+      (res) => {
+        // console.log(res.data[0].excuse);
+        setExcuse(res.data[0].excuse);
+      }
+    );
+  }
+
+  useEffect(generateRandomExcuse, []);
 
   return (
     <div className="App">
-      <button onClick={setToggle}>Toggle</button>
-      <button onClick={setTrue}>setTrue</button>
-      <button onClick={setFalse}>setFalse</button>
-
-      <>
-        {boolean === null || typeof boolean === "undefined"
-          ? "Not defined"
-          : boolean.toString()}
-      </>
+      <h2>Generate random excuses</h2>
+      <button
+        onClick={() => {
+          setExcuseType("party");
+          generateRandomExcuse();
+        }}
+      >
+        Party
+      </button>
+      <button
+        onClick={() => {
+          setExcuseType("family");
+          generateRandomExcuse();
+        }}
+      >
+        Family
+      </button>
+      <button
+        onClick={() => {
+          setExcuseType("office");
+          generateRandomExcuse();
+        }}
+      >
+        Office
+      </button>
+      <p>{excuse}</p>
     </div>
   );
 }
